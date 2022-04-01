fedc576af921d314d7e74c5e98159588174520c0
nndar <nndar@LAPTOP-NH4KCRRQ>
2022-04-01 17:09:52 +0530

    Add project files.

---
+++ BasicProgram/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace basicProgram
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number: ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            if (val % 2 == 0)
+            {
+                Console.WriteLine(val + " is Even number");
+            }
+            else
+            {
+                Console.WriteLine(val + " is Odd number");
+            }
+        }
+    }
+}
+
